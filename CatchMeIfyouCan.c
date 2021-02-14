da1587d1c90d414bf0bf6fadd210690f49a265cd
Anmol K Sachan <60771253+anmolksachan@users.noreply.github.com>
2021-02-15 02:52:01 +0530

    Create CatchMeIfyouCan.c

---
+++ CatchMeIfyouCan.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int a = 123321;
+  int key, flag = 0;
+  printf("Please Enter The Registration Keys\n");
+  scanf("%d",&key);
+  while(flag == 0)
+  {
+  if(key == a)
+  {
+         printf("Registration Successfull....\n");
+         flag = 1;
+  }
+  else
+  {
+      printf("Wrong Keys, Please Enter again.....\n\n");
+      scanf("%d",&key);
+      flag = 0;   
+  }
+  getch();
+}    	
+  return 0;
+}
